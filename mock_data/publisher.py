eb0b4ebe4b68fd469f2e79573a2a5c1d283b3ec3
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-09-19 10:50:41 -0300

    :dizzy: code: create basic publisher

--- mock_data/publisher.py
+++ mock_data/publisher.py
@@ -1,1 +1,13 @@
 from google.cloud import pubsub_v1
+
+project_id = 'big-maxim-430019-g7'
+topic_id = 'mockdata-for-bq'
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(project_id,topic_id)
+
+message = "new message to pubsub"
+
+future = publisher.publish(topic_path, message.encode('utf-8'))
+print(f'mensagem publicada: {future.result()}')
+
