c390fa558fd71f45bd7a25952a256119407cac8e
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-09-19 02:47:57 -0300

    :dizzy: code: update acces to env data

--- mock_data/main.py
+++ mock_data/main.py
@@ -1,9 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
+from config import settings
 from routes import router
 
-app = FastAPI()
+app = FastAPI(title=settings.API_TITLE, description=settings.API_DESCRIPTION, version=settings.API_VERSION)
 app.include_router(router)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='localhost', port=8000)
+    uvicorn.run(app, host=settings.API_HOST, port=settings.API_PORT)
