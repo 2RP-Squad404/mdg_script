14035a6bdfd7bd1c75fdb4b90bedcd06e999e89b
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-09-20 16:56:13 -0300

    💫 code: update models file

--- mock_data/models.py
+++ mock_data/models.py
@@ -58,23 +58,23 @@ class CardEvent(BaseModel):
     num_bin: str
     cod_loja_emis_cartao: str
     id_cliente_so: str
-    dth_emis_cartao: datetime
-    dth_embs_cartao: datetime
-    dth_valid_cartao: datetime
-    dth_desbloqueio: datetime
+    dth_emis_cartao: str
+    dth_embs_cartao: str
+    dth_valid_cartao: str
+    dth_desbloqueio: str
     cod_sit_cartao: str
     des_sit_cartao: str
-    dth_sit_cartao: datetime
+    dth_sit_cartao: str
     cod_estagio_cartao: str
     des_estagio_cartao: str
-    dth_estagio_cartao: datetime
+    dth_estagio_cartao: str
     flg_embs_loja: str
     flg_cartao_cancelado: str
     flg_cartao_provisorio: str
     flg_conta_cancelada: str
-    dth_ult_atu_so: datetime
+    dth_ult_atu_so: str
     num_seq_ult_alteracao: str
-    dth_inclusao_reg: datetime
+    dth_inclusao_reg: str
     pt_nomeplastico: str
     ca_arquivolote: str
     ca_id_imagem: str
