cb1d455c7c3f1884e087a7f75f3444f121293a11
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-09-20 16:56:34 -0300

    💫 code: update send data file

--- mock_data/send_data.py
+++ mock_data/send_data.py
@@ -1,14 +1,25 @@
-from datagen import generate_cardevent
-from bq_table import client, table_id
-
-# rows to insert in table 
-
-card_rows = [generate_cardevent()]
+from datagen import generate_account
+from google.cloud import bigquery
+from config import settings
 
 
-errors = client.insert_rows_json(table_id, generate_cardevent())
+project_id = settings.PROJECT_ID
+dataset_id = settings.DATASET_ID
+table_id = settings.TABLE_ID
+
+client = bigquery.Client(project=project_id)
+
+
+account_mock_data = [generate_account()]
+
+
+table_ref = client.dataset(dataset_id).table(table_id)
+
+
+errors = client.insert_rows_json(table_ref, account_mock_data)
+
 
 if errors == []:
-    print("Dados inseridos com sucesso.")
+    print("sucess send")
 else:
-    print(f"Erros ao inserir dados: {errors}")
+    print(f"Errors: {errors}")
