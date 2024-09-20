99f91cb8ca1cd78885f7e212ca9ee2de20852e4f
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-09-20 02:33:58 -0300

    💫 code: update datagen file for generate card event

--- mock_data/datagen.py
+++ mock_data/datagen.py
@@ -2,7 +2,7 @@ import random
 from datetime import date
 
 from faker import Faker
-from models import Account, BuyEvent, Card, Person
+from models import Account, BuyEvent, Card, Person, CardEvent
 
 fake = Faker(['pt_BR'])
 
@@ -20,9 +20,9 @@ def generate_person():
         cpf=fake.cpf()
     )
 
-    # person_dict = person.__dict__
+    person_dict = person.__dict__
 
-    return person
+    return person_dict
 
 
 def generate_account():
@@ -74,6 +74,48 @@ def generate_buy():
     buy_dict = buy.__dict__
 
     return buy_dict
+
+def generate_cardevent():
+    card_event = CardEvent(
+        id_cartao = fake.unique.random_number(digits=8),
+        id_produto_cartao = fake.uuid4(),  # Gerando um ID único como string
+        num_cartao = fake.credit_card_number(card_type="mastercard"),
+        num_seq_via_cartao = str(random.randint(1, 10)),
+        id_conta = str(random.randint(10000000, 99999999)),
+        num_cpf_cliente = fake.ssn(),
+        cod_tip_portador = str(random.randint(1, 5)),
+        num_bin = fake.credit_card_number()[:6],  # Pega os primeiros 6 dígitos do BIN
+        cod_loja_emis_cartao = str(random.randint(1000, 9999)),  # Pode ser None
+        id_cliente_so = str(random.randint(10000000, 99999999)),
+        dth_emis_cartao = fake.date_time_this_decade(),
+        dth_embs_cartao = fake.date_time_this_decade(),
+        dth_valid_cartao = fake.future_date(end_date="+8y"),
+        dth_desbloqueio = None,  # Pode ser None ou fake.date_time_this_decade()
+        cod_sit_cartao = str(random.randint(1, 5)),
+        des_sit_cartao = random.choice(["ATIVO", "BLOQUEADO", "CANCELADO"]),
+        dth_sit_cartao = fake.date_time_this_decade(),
+        cod_estagio_cartao = str(random.randint(1, 5)),
+        des_estagio_cartao = random.choice(["ENCAMINHADO", "FINALIZADO"]),
+        dth_estagio_cartao = fake.date_time_this_decade(),
+        flg_embs_loja = random.choice(["S", "N"]),
+        flg_cartao_cancelado = random.choice(["S", "N"]),
+        flg_cartao_provisorio = random.choice(["S", "N"]),
+        flg_conta_cancelada = random.choice(["S", "N", None]),
+        dth_ult_atu_so = fake.date_time_this_decade(),
+        num_seq_ult_alteracao = str(random.randint(1, 100)),
+        dth_inclusao_reg = fake.date_time_this_decade(),
+        pt_nomeplastico = fake.name(),
+        ca_arquivolote = fake.lexify(text="CPEM??????"),
+        ca_id_imagem = None,  # Pode ser None ou fake.uuid4()
+        bc_responsavel = fake.lexify(text="[IRIS]_????"),
+        ca_codigocancelamento = None,
+        ca_flaggeracartasenha = str(random.randint(0, 1)),
+        pt_id_imagem = None  # Pode ser None ou fake.uuid4()
+    )
+    card_event_dict = card_event.__dict__ 
+    
+    return card_event_dict
+
 
 
 def generate_event():
