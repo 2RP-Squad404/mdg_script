1e2a205dedea2f79289bff57a879737abc96f220
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-09-25 02:27:41 -0300

    💫 code: arranje files for prod test

--- mock_data/config.py
+++ mock_data/config.py
@@ -2,33 +2,15 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    """
-    Classe Settings armazena as configurações globais da API com valores padrões.
-    
-    Atributos:
-    - API_TITLE (str): Título da API exibido na documentação
-    - API_VERSION (str): Versão da API
-    - API_HOST (str): Endereço da API
-    - API_PORT (int): Porta da API
-    - API_DESCRIPTION (str): Breve descrição da API
-    """
-    API_TITLE: str
-    API_VERSION: str
-    API_HOST: str
-    API_PORT: int
-    API_DESCRIPTION: str
     PROJECT_ID: str
     DATASET_ID: str
     TABLE_ID: str
 
 
 settings = Settings(
-    API_TITLE="Mock-Data App",
-    API_VERSION="v1.0.0",
-    API_HOST="localhost",
-    API_PORT=8001,
-    API_DESCRIPTION="Esta aplicação gera e dispara eventos com dados mockados para serem usados na pipeline analítica BigQuery/Dataform",
+
+
     PROJECT_ID='big-maxim-430019-g7',
-    DATASET_ID='big-maxim-430019-g7.mockdatadataset',
-    TABLE_ID='big-maxim-430019-g7.mockdatadataset.account'
+    DATASET_ID='mockdatadataset',
+    TABLE_ID='account'
 )
--- mock_data/datagen.py
+++ mock_data/datagen.py
@@ -1,7 +1,7 @@
 import random
 from datetime import date
 from faker import Faker
-from models import Account, BuyEvent, Card, Person, CardEvent
+from models import Account, BuyEvent, Card, CardEvent, Person, Mock_test
 
 fake = Faker(['pt_BR'])
 
@@ -74,47 +74,60 @@ def generate_buy():
 
     return buy_dict
 
+
 def generate_cardevent():
     card_event = CardEvent(
-        id_cartao = fake.uuid4(),
-        id_produto_cartao = fake.uuid4(),  
-        num_cartao = fake.credit_card_number(card_type="mastercard"),
-        num_seq_via_cartao = str(random.randint(1, 10)),
-        id_conta = str(random.randint(10000000, 99999999)),
-        num_cpf_cliente = fake.ssn(),
-        cod_tip_portador = str(random.randint(1, 5)),
-        num_bin = fake.credit_card_number()[:6],  
-        cod_loja_emis_cartao = str(random.randint(1000, 9999)),  
-        id_cliente_so = str(random.randint(10000000, 99999999)),
-        dth_emis_cartao = fake.uuid4(),
-        dth_embs_cartao = fake.uuid4(),
-        dth_valid_cartao = fake.uuid4(),
-        dth_desbloqueio = fake.uuid4(),
-        cod_sit_cartao = str(random.randint(1, 5)),
-        des_sit_cartao = random.choice(["ATIVO", "BLOQUEADO", "CANCELADO"]),
-        dth_sit_cartao = fake.uuid4(),
-        cod_estagio_cartao = str(random.randint(1, 5)),
-        des_estagio_cartao = random.choice(["ENCAMINHADO", "FINALIZADO"]),
-        dth_estagio_cartao = fake.uuid4(),
-        flg_embs_loja = random.choice(["S", "N"]),
-        flg_cartao_cancelado = random.choice(["S", "N"]),
-        flg_cartao_provisorio = random.choice(["S", "N"]),
-        flg_conta_cancelada = random.choice(["S", "N"]),
-        dth_ult_atu_so = fake.uuid4(),
-        num_seq_ult_alteracao = str(random.randint(1, 100)),
-        dth_inclusao_reg = fake.uuid4(),
-        pt_nomeplastico = fake.name(),
-        ca_arquivolote = fake.lexify(text="CPEM??????"),
-        ca_id_imagem = fake.uuid4(),
-        bc_responsavel = fake.lexify(text="[IRIS]_????"),
-        ca_codigocancelamento = fake.uuid4(),
-        ca_flaggeracartasenha = str(random.randint(0, 1)),
-        pt_id_imagem = fake.uuid4()
+        id_cartao=fake.uuid4(),
+        id_produto_cartao=fake.uuid4(),
+        num_cartao=fake.credit_card_number(card_type="mastercard"),
+        num_seq_via_cartao=str(random.randint(1, 10)),
+        id_conta=str(random.randint(10000000, 99999999)),
+        num_cpf_cliente=fake.ssn(),
+        cod_tip_portador=str(random.randint(1, 5)),
+        num_bin=fake.credit_card_number()[:6],
+        cod_loja_emis_cartao=str(random.randint(1000, 9999)),
+        id_cliente_so=str(random.randint(10000000, 99999999)),
+        dth_emis_cartao=fake.uuid4(),
+        dth_embs_cartao=fake.uuid4(),
+        dth_valid_cartao=fake.uuid4(),
+        dth_desbloqueio=fake.uuid4(),
+        cod_sit_cartao=str(random.randint(1, 5)),
+        des_sit_cartao=random.choice(["ATIVO", "BLOQUEADO", "CANCELADO"]),
+        dth_sit_cartao=fake.uuid4(),
+        cod_estagio_cartao=str(random.randint(1, 5)),
+        des_estagio_cartao=random.choice(["ENCAMINHADO", "FINALIZADO"]),
+        dth_estagio_cartao=fake.uuid4(),
+        flg_embs_loja=random.choice(["S", "N"]),
+        flg_cartao_cancelado=random.choice(["S", "N"]),
+        flg_cartao_provisorio=random.choice(["S", "N"]),
+        flg_conta_cancelada=random.choice(["S", "N"]),
+        dth_ult_atu_so=fake.uuid4(),
+        num_seq_ult_alteracao=str(random.randint(1, 100)),
+        dth_inclusao_reg=fake.uuid4(),
+        pt_nomeplastico=fake.name(),
+        ca_arquivolote=fake.lexify(text="CPEM??????"),
+        ca_id_imagem=fake.uuid4(),
+        bc_responsavel=fake.lexify(text="[IRIS]_????"),
+        ca_codigocancelamento=fake.uuid4(),
+        ca_flaggeracartasenha=str(random.randint(0, 1)),
+        pt_id_imagem=fake.uuid4()
     )
-    card_event_dict = card_event.__dict__ 
-    
+    card_event_dict = card_event.__dict__
+
     return card_event_dict
 
+def generate_mock_data_test():
+    mock_test = Mock_test(
+        person_id = fake.unique.random_int(min=1, max=2002, step=1),
+        name = fake.name()
+    )
+    
+    mock_test_dict = mock_test.__dict__
+    
+    return mock_test_dict
+
+mock_tests = [generate_mock_data_test() for i in range(10)]
+print(mock_tests)
 
 
 # def generate_event():
