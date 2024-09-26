37ae1beaaa6fc8373efa75eecd65ca7a8ba63632
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-09-26 17:46:09 -0300

    💫 code: create code for get schemas of existent datasets and tables

---
+++ mock_data/get_schemas.py
@@ -0,0 +1,22 @@
+import os
+
+from google.cloud import bigquery
+
+Table_client = bigquery.Client()
+schema_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'schemas.py')
+
+card_table_id = Table_client.get_table(str(input("Insira o ID do seu Dataset e de sua tabela no formato 'dataset_id.table_id' : ")))
+
+unformat_card_table_schema = card_table_id.schema
+
+# print("Schema sem a formatação correta: \n")
+# print(unformat_card_table_schema)
+
+formated_card_schema = {field.name: field.field_type for field in unformat_card_table_schema}
+# print("\nSchema com a formatação correta: \n")
+# print(formated_card_schema)
+
+card_schema = "card_schema = "
+with open(schema_file_path, 'a') as file:
+    file.write(card_schema + str(formated_card_schema))
+    print("Schema importado com sucesso!")
