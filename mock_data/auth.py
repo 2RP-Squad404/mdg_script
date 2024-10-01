ccd5d53b3b187b9b4820ae1ee0766d628a0cad49
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-10-01 02:05:50 -0300

    💫 code: add auth file using secret manager

--- mock_data/auth.py
+++ mock_data/auth.py
@@ -1,14 +1,15 @@
 import json
-
 from google.cloud import bigquery, secretmanager
 from google.oauth2 import service_account
+
+# Primeira tentativa de autenticação com Secret Manager: 
 
 secret_id = "bq-credential"
 project_id = "big-maxim-430019-g7"
 
 secret_client = secretmanager.SecretManagerServiceClient()
 
-secret_name = "projects/big-maxim-430019-g7/secrets/bq-credential/versions/latest"
+secret_name = "projects/222448658386/secrets/bq-credential/versions/latest"
 response = secret_client.access_secret_version(name=secret_name)
 
 secret_payload = response.payload.data.decode("UTF-8")
@@ -16,3 +17,4 @@ credentials_info = json.loads(secret_payload)
 
 credentials = service_account.Credentials.from_service_account_info(credentials_info)
 client = bigquery.Client(credentials=credentials, project=project_id)
+
