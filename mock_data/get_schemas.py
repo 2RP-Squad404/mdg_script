430f85b6afba93d1b1452a29f578ff10f0e7a4b7
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-10-03 17:34:13 -0300

    💫 code: build new schemas for mock table

--- mock_data/get_schemas.py
+++ mock_data/get_schemas.py
@@ -1,7 +1,10 @@
 import os
 
 # from auth import client
-from main import client
+# from main import client
+from google.cloud import bigquery
+
+client = bigquery.Client()
 
 schema_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'schemas.py')
 
@@ -9,12 +12,12 @@ card_table_id = client.get_table(str(input("Insira o ID do seu Dataset e de sua 
 
 unformat_card_table_schema = card_table_id.schema
 
-# print("Schema sem a formatação correta: \n")
-# print(unformat_card_table_schema)
+print("Schema sem a formatação correta: \n")
+print(unformat_card_table_schema)
 
 formated_card_schema = {field.name: field.field_type for field in unformat_card_table_schema}
-# print("\nSchema com a formatação correta: \n")
-# print(formated_card_schema)
+print("\nSchema com a formatação correta: \n")
+print(formated_card_schema)
 
 card_schema = "card_schema = "
 with open(schema_file_path, 'a') as file:
