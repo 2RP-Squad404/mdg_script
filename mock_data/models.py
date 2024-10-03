318d6864588d6c634b48d90a08027c3b7c8ab8eb
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-10-03 01:49:02 -0300

    💫 code: make simple relation of fake data

--- mock_data/models.py
+++ mock_data/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from pydantic import BaseModel
 
 
@@ -36,3 +38,34 @@ class CardEvent(BaseModel):
     ca_codigocancelamento: str
     ca_flaggeracartasenha: str
     pt_id_imagem: str
+
+
+# Teste de mock dados com relacionamento
+
+class Card(BaseModel):
+        card_id: int
+        card_number: str
+        account_id: int
+        status_id: int
+        limit: float
+        expiration_date: str
+
+
+class Account(BaseModel):
+    account_id: int
+    status_id: int
+    due_day: int
+    person_id: int
+    balance: float
+    avaliable_balance: float
+
+
+class Person(BaseModel):
+    person_id: int
+    name: str
+    email: str
+    gender: str
+    birth_date: date
+    address: str
+    salary: float
+    cpf: str
