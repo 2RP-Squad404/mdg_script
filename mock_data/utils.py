91057dd34f239cb0ba564558d106fd6588ab5069
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-10-03 01:49:37 -0300

    💫 code: create utils file

---
+++ mock_data/utils.py
@@ -0,0 +1,14 @@
+# Este arquivo possui a implementação de funções auxiliares da aplicação
+# Como por exemplo: Criar tabelas, Extrair schemas e Extrair dados
+
+# Criar uma tabela a partir de um schema
+
+# ...
+
+
+
+
+
+# Extrair um schema de uma tabela já existente
+
+# ...
