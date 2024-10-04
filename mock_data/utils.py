a9771731a3532009ee528df4de7b82bb05c20af5
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-10-04 01:09:06 -0300

    🔧 chore: linter format

--- mock_data/utils.py
+++ mock_data/utils.py
@@ -1,11 +1,14 @@
 # Este arquivo possui a implementação de funções auxiliares da aplicação
 # Como por exemplo: Criar tabelas, Extrair schemas e Extrair dados
 
+from config import settings
 from google.api_core.exceptions import NotFound
 from google.cloud import bigquery
-
-from config import settings
-from schemas import bigquery_schema_account, bigquery_schema_card, bigquery_schema_person
+from schemas import (
+    bigquery_schema_account,
+    bigquery_schema_card,
+    bigquery_schema_person,
+)
 
 # Criar uma tabela a partir de um schema
 
@@ -18,13 +21,13 @@ project_id = settings.PROJECT_ID
 # ID do dataset
 dataset_name = settings.PFS_UNIFICACAO_PEFISA_DATASET_ID
 
-# Nome das tabelas 
+# Nome das tabelas
 table_person = f"{project_id}.{dataset_name}.mock_simple_persons"
 table_account = f"{project_id}.{dataset_name}.mock_simple_account"
 table_card = f"{project_id}.{dataset_name}.mock_simple_card"
 
 
-def check_and_create_table(client, table_id, schema):
+def create_table(client, table_id, schema):
     try:
         client.get_table(table_id)  # Tenta pegar a tabela
         print(f"Tabela {table_id} já existe.")
@@ -34,7 +37,7 @@ def check_and_create_table(client, table_id, schema):
         client.create_table(table)
         print(f"Tabela {table_id} criada com sucesso.")
 
-
-check_and_create_table(client, table_person, bigquery_schema_person)
-check_and_create_table(client, table_account, bigquery_schema_account)
-check_and_create_table(client, table_card, bigquery_schema_card)
+# 
+create_table(client, table_person, bigquery_schema_person)
+create_table(client, table_account, bigquery_schema_account)
+create_table(client, table_card, bigquery_schema_card)
