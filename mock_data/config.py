77aa9f9acbd024acddb420f0078c5625620b7775
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-10-06 03:30:04 -0300

    :books: docs: write docstring for config class

--- mock_data/config.py
+++ mock_data/config.py
@@ -2,6 +2,22 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    """
+    !Atenção: Esta classe esta projetada para um ambiente de teste, haverá mudanças 
+    quando esta aplicação estiver em ambiente de produção. 
+    
+    Classe Settings é uma classe que armazena os valores de configuração da aplicação.
+    
+    Atributos:
+    
+        PROJECT_ID (str): O ID do projeto GCP onde os recursos estão localizados.
+        
+        PFS_UNIFICACAO_PEFISA_DATASET_ID (str): O ID do dataset no BigQuery que está sendo utilizado nos testes
+        em desenvolvimento.
+        
+        MOCK_CARD_TABLE_ID (str): O ID de uma das tabelas de teste para armazenar dados mockados
+            de cartões no BigQuery.
+    """
     PROJECT_ID: str
     PFS_UNIFICACAO_PEFISA_DATASET_ID: str
     MOCK_CARD_TABLE_ID: str
