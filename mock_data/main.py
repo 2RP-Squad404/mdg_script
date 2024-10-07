1c0925c7477bbcd875dbcf88890098c6bae6989c
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-10-07 02:12:02 -0300

    💫 code: add main menu and choice 1 logic

--- mock_data/main.py
+++ mock_data/main.py
@@ -1,42 +1,49 @@
-import time
 
-from config import settings
-from datagen import generate_cardevent
-
-# from auth import client
 from google.cloud import bigquery
+import sys
+from utils import cli_start, main_menu, create_class_code, import_table_schema, write_class_to_file
 
-start = time.time()
+# start = time.time()
 client = bigquery.Client()
 
-num_of_lines = input("Quantas linhas você deseja inserir na tabela Card? ")
+cli_start()
+
+while True:
+    choice = main_menu()
+
+    if choice == "1":
+            print("\nOpção 1 selecionada: Importar um schema do BigQuery\n")
+            dataset = input("Insira o ID do dataset:\n")
+            table = input("Insira o ID da tabela:\n")
+            import_table_schema(client=client,dataset_id=dataset,table_id=table)
+            print("\n")
+    elif choice == "2":
+            print("\nOpção 2 selecionada: Exibir tabelas\n")
+    elif choice == "3":
+            print("\nOpção 3 selecionada: Enviar dados\n")
+    elif choice == 'S' or choice == 's':
+            print('Encerrando aplicação...')
+            sys.exit()
+    else:
+            print("\nOpção inválida. Tente novamente.\n")
+
+# num_of_lines = input("Quantas linhas você deseja inserir na tabela Card? ")
+# def send_to_card_table(card_mock_data, dataset_id, table_id):
+#     table_ref = client.dataset(dataset_id).table(table_id)
+#     table = client.get_table(table_ref)
+#     line_mock_data = [card_mock_data]
+#     errors = client.insert_rows_json(table, line_mock_data)
+#     if errors:
+#         print(f"Erro ao enviar: {errors}")
+# card_mock_data = []
+# for i in range(int(num_of_lines)):
+#     card_mock_data.append(generate_cardevent())
+# dataset_id = settings.PFS_UNIFICACAO_PEFISA_DATASET_ID
+# table_id = settings.MOCK_CARD_TABLE_ID
+# for k in range(int(num_of_lines)):
+#     send_to_card_table(card_mock_data[k], dataset_id, table_id)
+#     print("Enviando dados..." + f"Restam {int(num_of_lines) - k} linhas.")
+# end_code = time.time()
+# print(f"Envio dos dados finalizado em {(end_code - start):.2f}ms")
 
 
-def send_to_card_table(card_mock_data, dataset_id, table_id):
-
-    table_ref = client.dataset(dataset_id).table(table_id)
-    table = client.get_table(table_ref)
-
-    line_mock_data = [card_mock_data]
-    errors = client.insert_rows_json(table, line_mock_data)
-
-    if errors:
-        print(f"Erro ao enviar: {errors}")
-
-
-card_mock_data = []
-
-for i in range(int(num_of_lines)):
-    card_mock_data.append(generate_cardevent())
-
-dataset_id = settings.PFS_UNIFICACAO_PEFISA_DATASET_ID
-table_id = settings.MOCK_CARD_TABLE_ID
-
-
-for k in range(int(num_of_lines)):
-    send_to_card_table(card_mock_data[k], dataset_id, table_id)
-    print("Enviando dados..." + f"Restam {int(num_of_lines) - k} linhas.")
-
-end_code = time.time()
-
-print(f"Envio dos dados finalizado em {(end_code - start):.2f}ms")
