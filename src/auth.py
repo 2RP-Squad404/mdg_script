dc0e07c9d01abbf346d6b38150a0c63553fdd4e7
Kelvin Silvestre <kelvinsc7@gmail.com>
2024-10-09 13:22:12 -0300

    :books: docs: auth and config docstrings

--- src/auth.py
+++ src/auth.py
@@ -3,8 +3,27 @@ from google.oauth2 import service_account
 import json
 from config import PROJECT_ID, SECRET_NAME
 
-# Função para buscar o segredo no Secret Manager
 def get_secret(secret_name, project_id):
+
+    """
+    Busca um segredo específico no Secret Manager do Google Cloud.
+
+    Esta função acessa o Secret Manager, utilizando o nome do segredo e o ID do projeto,
+    e retorna o valor do segredo como um dicionário JSON.
+
+    Parâmetros:
+        secret_name (str): O nome do segredo a ser recuperado.
+        project_id (str): O ID do projeto do Google Cloud.
+
+    Retorna:
+        dict: O segredo decodificado como um dicionário JSON, se encontrado.
+        None: Se houver um erro durante a recuperação do segredo.
+
+    Exceções:
+        Exception: Qualquer erro durante o acesso ao Secret Manager será tratado e 
+        uma mensagem de erro será exibida no console.
+    """
+
     try:
         client = secretmanager.SecretManagerServiceClient()
         name = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
@@ -15,15 +34,28 @@ def get_secret(secret_name, project_id):
         print(f"Erro ao buscar o segredo do Secret Manager: {e}")
         return None
 
-# Função para autenticar no BigQuery usando as credenciais do Secret Manager
 def get_bigquery_client():
+
+    """
+    Autentica no BigQuery usando as credenciais armazenadas no Secret Manager.
+
+    Esta função obtém as credenciais do Secret Manager, cria um objeto de credenciais
+    e retorna um cliente autenticado do BigQuery.
+
+    Retorna:
+        bigquery.Client: Um cliente autenticado para interagir com o BigQuery.
+        None: Se houver um erro ao buscar as credenciais ou ao criar o cliente.
+
+    Exceções:
+        Exception: Qualquer erro durante a autenticação ou criação do cliente será
+        tratado e uma mensagem de erro será exibida no console.
+    """
+    
     try:
-        # Busca as credenciais do Secret Manager
         credentials_info = get_secret(SECRET_NAME, PROJECT_ID)
         if credentials_info is None:
             raise Exception("Credenciais não encontradas no Secret Manager.")
         
-        # Cria as credenciais com as informações recuperadas
         credentials = service_account.Credentials.from_service_account_info(credentials_info)
         
         # Retorna o cliente do BigQuery autenticado
--- src/config.py
+++ src/config.py
@@ -1,5 +1,20 @@
 from pydantic_settings import BaseSettings
 
+"""
+    Classe Settings para gerenciar a configuração do projeto usando variáveis de ambiente.
+
+    Esta classe utiliza o BaseSettings do Pydantic para ler e analisar automaticamente
+    as variáveis de ambiente de um arquivo `.env`, mapeando-as para atributos de fácil acesso.
+
+    Atributos:
+        project_id (str): O ID do projeto no Google Cloud.
+        secret_name (str): O nome do segredo armazenado no Secret Manager.
+
+    Config:
+        env_file (str): Caminho para o arquivo de ambiente, que pode ser "../.env" ou ".env".
+        extra (str): Define que qualquer variável de ambiente adicional deve ser ignorada.
+"""
+
 class Settings(BaseSettings):
     project_id: str
     secret_name: str
