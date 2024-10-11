862e89482a9ff15786a732bcc67e6ca601122b03
Henrique Fernandes <henriquealcalde@hotmail.com>
2024-10-11 14:32:32 -0300

    :sparkles:feat:add new functions to add columns to tables

--- src/main.py
+++ src/main.py
@@ -2,19 +2,22 @@
 import sys
 
 from google.cloud import bigquery
-from utils import cli_start, import_table_schema, main_menu, show_tables, write_class_to_file
-project_id = 'sapient-cycling-434419-u0'
+from utils import cli_start, import_table_schema, main_menu, show_tables,inspect_table_schema, write_class_to_file,get_all_schemas,create_tables_with_schemas
+# project_id = 'sapient-cycling-434419-u0'
 
-client = bigquery.Client(project = project_id)
+client = bigquery.Client(project="just-lore-435816-v8")
+dataset_id = 'just-lore-435816-v8.json_data'
+
 
 cli_start()
 main_menu()
 
 while True:
-    choice = input("\nEscolha uma opção (1-3): \n" + "> ")
+    choice = input("\nEscolha uma opção (1-4): \n" + "> ")
     print("1. Importar schema do BigQuery")
     print("2. Exibir tabelas")
     print("3. Enviar dados")
+    print("4. Criar tabelas via json")
     print('S. Sair')
     if choice == "1":
             print("\nOpção 1 selecionada: Importar um schema do BigQuery\n")
@@ -28,6 +31,13 @@ while True:
             show_tables()
     elif choice == "3":
             print("\nOpção 3 selecionada: Enviar dados\n")
+    elif choice == "4":
+            print("\nOpção 4 selecionada: Criar tabelas via json\n")
+            schemas = get_all_schemas("json_files")
+            create_tables_with_schemas(schemas, dataset_id)
+            for schema_info in schemas:
+                table_name = schema_info['filename'].replace('.json', '')
+                inspect_table_schema(dataset_id, table_name)
     elif choice == 'S' or choice == 's':
             print('Encerrando aplicação...')
             sys.exit()
