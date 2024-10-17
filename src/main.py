8875fcd56d57d9ff3df5d4e0a98b98bd16eb7775
Henrique Fernandes <henriquealcalde@hotmail.com>
2024-10-17 15:45:32 -0300

    :sparkles:feat:modify the functions to official project

--- src/main.py
+++ src/main.py
@@ -1,12 +1,10 @@
-
 import sys
 
 from google.cloud import bigquery
-from utils import cli_start, import_table_schema, main_menu, show_tables,inspect_table_schema, write_class_to_file,get_all_schemas,create_tables_with_schemas
+from config import PROJECT_ID
+from utils import cli_start, import_table_schema, main_menu, get_tables,write_class_to_file,get_all_schemas,create_tables_with_schemas
 
-client = bigquery.Client(project="just-lore-435816-v8")
-dataset_id = 'just-lore-435816-v8.json_data'
-
+client = bigquery.Client(PROJECT_ID)
 
 cli_start()
 main_menu()
@@ -27,16 +25,27 @@ while True:
             print("\n")
     elif choice == "2":
             print("\nOpção 2 selecionada: Exibir tabelas\n")
-            show_tables()
+            get_tables()
     elif choice == "3":
             print("\nOpção 3 selecionada: Enviar dados\n")
     elif choice == "4":
-            print("\nOpção 4 selecionada: Criar tabelas via json\n")
-            schemas = get_all_schemas("json_files")
-            create_tables_with_schemas(schemas, dataset_id)
-            for schema_info in schemas:
-                table_name = schema_info['filename'].replace('.json', '')
-                inspect_table_schema(dataset_id, table_name)
+            while True:
+                print("\nOpção 4 selecionada: Criar tabelas via json\n")
+                dataset_number = input("\nSelecione qual dataset irá ser gerado os dados\n1. pfs_raw_conductor\n2. pfs_unificacao_pefisa\n")
+                
+                if dataset_number == "1":
+                        directory_schema = "bq_schemas/pfs_raw_conductor"
+                        dataset_id = 'pfs_raw_conductor'
+                elif dataset_number == "2":
+                        directory_schema = "bq_schemas/pfs_unificacao_pefisa"
+                        dataset_id = 'pfs_unificacao_pefisa'
+                else:
+                        print("\nOpção inválida")
+                
+                schemas = get_all_schemas(directory_schema)
+                create_tables_with_schemas(schemas, dataset_id)
+                for schema_info in schemas:
+                        table_name = schema_info['filename'].replace('.json', '')
     elif choice == 'S' or choice == 's':
             print('Encerrando aplicação...')
             sys.exit()
