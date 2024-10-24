7be3dd413d96214b5a26ca5f68ba3a618927ab59
Kelvin Silvestre <kelvinsc7@gmail.com>
2024-10-24 16:39:37 -0300

    :bug: fix: logging error

--- src/test_connection.py
+++ src/test_connection.py
@@ -1,19 +1,19 @@
+import logging
 from auth import get_bigquery_client
 
-# Inicializa o cliente do BigQuery
+logging.basicConfig(level=logging.INFO,format='%(levelname)s: %(message)s')
+
 client = get_bigquery_client()
 
 if client is not None:
     try:
-        # Executa uma query simples para verificar a autenticação
         query = "SELECT current_date() as today"
         query_job = client.query(query)
 
-        # Verifica se a query foi executada com sucesso
         for row in query_job:
-            print(f"Conexão bem-sucedida! Data de hoje: {row['today']}")
-            print("BigQuery está autenticado com a conta de serviço.")
+            logging.info(f"Conexão bem-sucedida! Data de hoje: {row['today']}")
+            logging.info("Autenticado com a conta de serviço.")
     except Exception as e:
-        print(f"Erro ao executar a query: {e}")
+        logging.error(f"Erro ao executar a query: {e}")
 else:
-    print("Falha ao autenticar o cliente do BigQuery.")
+    logging.error("Falha ao autenticar.")
