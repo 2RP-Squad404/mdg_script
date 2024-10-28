e5cc692289ea0dbddb52cb3988608b9b02f260c9
Henrique Fernandes <henriquealcalde@hotmail.com>
2024-10-28 13:25:49 -0300

    :sparkles:feat:add jsonl_to_bigquery function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 from google.cloud import bigquery
 from config import PROJECT_ID
-from utils import create_tables
+from utils import create_tables,jsonl_to_bigquery
 
 client = bigquery.Client(PROJECT_ID)
-import csv
 # client = bigquery.Client('big-maxim-430019-g7')
 
 directory = './bq_schemas'
@@ -11,6 +10,7 @@ directory = './bq_schemas'
 create_tables()
 # - Criar dataset e tabelas
 # ...
-# - Enviar dados via csv
+# - Enviar dados via jsonl
+jsonl_to_bigquery
 # ...
 
