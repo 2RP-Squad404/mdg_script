04527b393994741260247851ab5db06c3e7ca37f
rafael-rodrigues01 <rafa_rodrigues01@yahoo.com>
2024-11-19 13:34:21 -0300

    🧹 cleanup: ruff format

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
-
 from pathlib import Path
 import sys
 
 from config import PROJECT_ID, SECRET_NAME, logger
-from utils import display_common_datasets, create_tables, run_command, jsonl_data, send_jsonl_to_bigquery
+from utils import (
+    display_common_datasets,
+    create_tables,
+    run_command,
+    jsonl_data,
+    send_jsonl_to_bigquery,
+)
 from generate_models import create_pydantic_models
 from gemini_interface import run_gemini
+
 
 def cli_option():
     bq_schemas_path = Path(__file__).resolve().parent / 'bq_schemas'
@@ -13,43 +19,49 @@ def cli_option():
     # logger.debug(bq_schemas_path)
 
     create_pydantic_models(str(bq_schemas_path))
-    
-    logger.info("Funções possiveis:")
-    logger.info("1 - Criar tabelas por dataset no BigQuery")
-    logger.info("2 - Gerar funções Faker com Gemini")
-    logger.info("3 - Gerar dados em JSONL")
-    logger.info("4 - Enviar JSONL para o BigQuery")
 
-    input_user = input("Escolha uma opção: ")
+    logger.info('Funções possiveis:')
+    logger.info('1 - Criar tabelas por dataset no BigQuery')
+    logger.info('2 - Gerar funções Faker com Gemini')
+    logger.info('3 - Gerar dados em JSONL')
+    logger.info('4 - Enviar JSONL para o BigQuery')
 
-    match(input_user):
-        case "1":
+    input_user = input('Escolha uma opção: ')
+
+    match input_user:
+        case '1':
             select_dataset = display_common_datasets(folder_path='bq_schemas')
             create_tables(select_dataset)
-        case "2":
+        case '2':
             select_dataset = display_common_datasets(folder_path='bq_schemas')
-            run_gemini(project_id=PROJECT_ID, model_name="gemini-1.5-flash-002",dataset=select_dataset)
-        case "3":
+            run_gemini(
+                project_id=PROJECT_ID,
+                model_name='gemini-1.5-flash-002',
+                dataset=select_dataset,
+            )
+        case '3':
             select_dataset = display_common_datasets(folder_path='bq_schemas')
             data = run_command(f'python {select_dataset}.py')
             jsonl_data(data)
-        case "4":
+        case '4':
             select_dataset = display_common_datasets(folder_path='mock_data')
             send_jsonl_to_bigquery(select_dataset)
 
+
 def run_cli():
     logger.info(
-        '\033[33mIdentifying active Google Cloud CLI authentication...\033[0m')
+        '\033[33mIdentifying active Google Cloud CLI authentication...\033[0m'
+    )
 
     project_id = PROJECT_ID
-    
+
     if not project_id:
         logger.info('\033[91mPROJECT ID: None\033[0m\n')
         sys.exit(1)
     logger.info(f'\033[32mPROJECT ID: {project_id}\033[0m\n')
 
     secret_name = SECRET_NAME
-    
+
     if not secret_name:
         logger.info('\033[91mSECRET NAME: None\033[0m\n')
         sys.exit(1)
@@ -58,5 +70,6 @@ def run_cli():
     logger.info('\033[33mAutenticating with Secret manager...\033[0m')
 
     cli_option()
+
 
 run_cli()
