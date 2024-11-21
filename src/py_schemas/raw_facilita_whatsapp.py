078bc23d5aca9283a0a0b0f380160ebfbca00d83
Henrique Fernandes <henriquealcalde@hotmail.com>
2024-11-21 19:18:02 -0300

    :package:build:new shcema file

--- src/py_schemas/raw_facilita_whatsapp.py
+++ src/py_schemas/raw_facilita_whatsapp.py
@@ -2,16 +2,16 @@ from google.cloud import bigquery
 
 # Dataset: raw_facilita_whatsapp, Table: revendedor
 revendedor = [
-    bigquery.SchemaField('id_revendedor', 'INTEGER', 'NULLABLE'),
-    bigquery.SchemaField('nome_revendedor', 'STRING', 'NULLABLE'),
-    bigquery.SchemaField('cpf_cnpj_revendedor', 'STRING', 'NULLABLE'),
-    bigquery.SchemaField('telefone_revendedor', 'STRING', 'NULLABLE'),
-    bigquery.SchemaField('email_revendedor', 'STRING', 'NULLABLE'),
-    bigquery.SchemaField('datahora_cadastro', 'TIMESTAMP', 'NULLABLE'),
-    bigquery.SchemaField('datahora_aceite', 'TIMESTAMP', 'NULLABLE'),
-    bigquery.SchemaField('cep', 'STRING', 'NULLABLE'),
-    bigquery.SchemaField('cod_loja', 'STRING', 'NULLABLE'),
-    bigquery.SchemaField('dth_inc', 'TIMESTAMP', 'NULLABLE'),
-    bigquery.SchemaField('source', 'STRING', 'NULLABLE'),
-    bigquery.SchemaField('production_date', 'DATE', 'NULLABLE')
+    bigquery.SchemaField('id_revendedor', 'INTEGER', 'NULLABLE', description='None'),
+    bigquery.SchemaField('nome_revendedor', 'STRING', 'NULLABLE', description='None'),
+    bigquery.SchemaField('cpf_cnpj_revendedor', 'STRING', 'NULLABLE', description='None'),
+    bigquery.SchemaField('telefone_revendedor', 'STRING', 'NULLABLE', description='None'),
+    bigquery.SchemaField('email_revendedor', 'STRING', 'NULLABLE', description='None'),
+    bigquery.SchemaField('datahora_cadastro', 'TIMESTAMP', 'NULLABLE', description='None'),
+    bigquery.SchemaField('datahora_aceite', 'TIMESTAMP', 'NULLABLE', description='None'),
+    bigquery.SchemaField('cep', 'STRING', 'NULLABLE', description='None'),
+    bigquery.SchemaField('cod_loja', 'STRING', 'NULLABLE', description='None'),
+    bigquery.SchemaField('dth_inc', 'TIMESTAMP', 'NULLABLE', description='None'),
+    bigquery.SchemaField('source', 'STRING', 'NULLABLE', description='None'),
+    bigquery.SchemaField('production_date', 'DATE', 'NULLABLE', description='None')
 ]
