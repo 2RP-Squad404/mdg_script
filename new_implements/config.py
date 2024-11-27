0a1143cb621bc1a6621da1b2822aec8fcde3b820
rafael-rodrigues01 <rafa_rodrigues01@yahoo.com>
2024-11-27 16:50:13 -0300

    🗑 remove: removed unnecessary file

--- new_implements/config.py
+++
@@ -1,6 +0,0 @@
-import logging.config
-import os
-
-config_path = os.path.join(os.path.dirname(__file__), 'logging.ini')
-logging.config.fileConfig(config_path)
-logger = logging.getLogger('appLogger')
